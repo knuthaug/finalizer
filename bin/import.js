8a0fd30213e9f859cd12c629bce40e2b1312fa3c
Knut Haugen <knuthaug+github@gmail.com>
2016-01-07 22:21:17 +0100

    rudimentary db import of years

--- bin/import.js
+++ bin/import.js
@@ -2,13 +2,20 @@
 
 var parser = require('../lib/parser.js'),
     sum = require('../lib/sum.js'),
-    db = require('../lib/db.js'),
-    file = process.argv[2];
-    year = process.argv[3];
+    Db = require('../lib/db.js'),
+    file = process.argv[2],
+    year = process.argv[3],
+    account = process.argv[4];
 
 parser.parseFile(file, storeYear);
 
 function storeYear(yearData) {
-    
+    var db = new Db('localhost');
+    db.storeYear(year, account, yearData).then(function(reply){
+        console.log("stored data to database");
+        process.exit();
+    }).catch(function(error){
+        console.error("Error when saving data to database:" + error);
+    });
 }
 
